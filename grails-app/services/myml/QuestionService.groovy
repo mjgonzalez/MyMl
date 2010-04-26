797e048949b1c729add3ab1e7e8a859a311f547b
pdigregorio <pdigregorio@ubuntu.ubuntu-domain>
2010-04-26 11:56:37 -0300

    Nueva super forma de acceder a la base!

--- grails-app/services/myml/QuestionService.groovy
+++ grails-app/services/myml/QuestionService.groovy
@@ -5,7 +5,12 @@ import myml.Question;
 
 class QuestionService {
 	Collection<Question> getPendingQuestions(Long sellerId){
-		Question.findAll("from Question as q where q.sellerId = ? and answerText is null", sellerId)
+		//OPCION 1:		Question.findAll("from Question as q where q.sellerId = ? and answerText is null", sellerId)
+		//OPCION 2: Muy Agile y scriptosa
+		Question.createCriteria().list {
+			eq("sellerId", sellerId)
+			isNull("answerText")
+		}
 	}
 	
 	void answerQuestion(Long questionId, String answerText){
