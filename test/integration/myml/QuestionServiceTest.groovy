8da82822fa21d48e20b814c9d8bb706b76835f21
pdigregorio <pdigregorio@ubuntu.ubuntu-domain>
2010-04-27 17:26:07 -0300

    Agregado Testeo para cuando se responde 1 pregunta de una lista

--- test/integration/myml/QuestionServiceTest.groovy
+++ test/integration/myml/QuestionServiceTest.groovy
@@ -4,7 +4,7 @@ import grails.test.GrailsUnitTestCase
 import myml.QuestionService
 import myml.Question
 
-class QuestionServiceTest extends GroovyTestCase{
+class QuestionServiceTest extends GrailsUnitTestCase{
 	def questionService
 	
 	void testEmptyQuestions(){
@@ -17,5 +17,49 @@ class QuestionServiceTest extends GroovyTestCase{
 		Question.findAllBySellerId(1)*.delete
 		
 		assertTrue questionService.getPendingQuestions(1).isEmpty()
+	}
+	
+	void testAnswerOneQuestionOfAList(){
+//		INVESTIGAR SOBRE MOCKS!!		
+//		mockDomain(Answer)
+//		mockDomain(Question)
+//		mockDomain(QuestionAction)
+//		mockDomain(Item)
+		
+		//Borramos todos los registros
+		Question.findAllBySellerId(1)*.delete
+		
+		def i = new Item(siteId:"MLA", title:"Item de Testeo CASO Una sola pregunta")
+		i.save()
+		
+		
+		def question = new Question(sellerId:1, askerNickname:"TESTMLA003",
+									questionText:"Tenés stock?", item:i)
+		question.save()
+		
+		//
+		def question2 = new Question(sellerId:1, askerNickname:"TESTMLA003",
+									questionText:"Tenés stock?", item:i)
+		question2.save()
+		
+		//Comprobamos que haya dos preguntas pendientes
+		assertEquals 2, Question.findAllBySellerId(1).size()
+		
+		//Respondemos la pregunta
+		questionService.answerQuestion(question.id, "No me queda más nada")
+		
+		//Comprobamos que exista la respuesta
+		def answer = Answer.findByQuestion(question)
+		
+		assertEquals answer.answerText, "No me queda más nada"
+		assertEquals answer.question.id, question.id
+		
+		//Comprobamos que exista la acción realizada
+		def questionAction = QuestionAction.findByQuestion(question)
+		assertEquals questionAction.actionType, "ANSWER"
+		assertEquals questionAction.question.id, question.id
+		
+		//Comprobamos que quede 1 pregunta por responder
+		assertEquals 1, questionService.getPendingQuestions(1).size()
 	}
 }
