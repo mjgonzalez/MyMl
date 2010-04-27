efcecff4e9d85e0209d5b5111346696758f1a9af
nsuarezmeli <nicolas.suarez@mercadolibre.com>
2010-04-27 17:02:47 -0300

    se modifica data source para agregar que use hsqldb para test se suben un par de tests

--- test/integration/myml/QuestionServiceTest.groovy
+++ test/integration/myml/QuestionServiceTest.groovy
@@ -5,17 +5,50 @@ import myml.QuestionService
 import myml.Question
 
 class QuestionServiceTest extends GroovyTestCase{
-	def questionService
+	QuestionService questionService
 	
+	/*
+	 * Testea que si el usuario no tiene preguntas, el servicio no devuelva nada
+	 */
 	void testEmptyQuestions(){
-		Question.findAllBySellerIdAndProcessed(1, false)
-		
-		assertTrue "No tiene preguntas", questionService.getPendingQuestions(1).isEmpty()
+		Question.findAllBySellerId(1)*.delete
+				
+		assertTrue questionService.getPendingQuestions(1).isEmpty()
 	}
 
+	/*
+	 * Testeo responder pregunta
+	 */
 	void testNotPendingQuestions(){
+//		mockDomain(Question)
+//		mockDomain(Answer)
+//		mockDomain(QuestionAction)
+//		mockDomain(Item)
+		
 		Question.findAllBySellerId(1)*.delete
 		
-		assertTrue questionService.getPendingQuestions(1).isEmpty()
+		Item testItem = new Item(siteId:"MLA", title:"Item de testeo")
+		testItem.save()
+		
+		Question q = new Question()
+		q.sellerId = 2
+		q.askerNickname = "AGILE"
+		q.questionText = "Tenes stock?"
+		q.item = testItem
+		
+		q.save()
+		
+		assertFalse questionService.getPendingQuestions(2).isEmpty()
+		
+		String answerText = "Todo bien vos?"
+		questionService.answerQuestion(q.id, answerText)
+		
+		QuestionAction answerAction = QuestionAction.findByQuestion(q)
+		assertNotNull answerAction
+		assertEquals "ANSWER", answerAction.actionType
+		
+		Answer answer = Answer.findByQuestion(q)
+		assertNotNull answer
+		assertEquals answerText, answer.answerText
 	}
 }
