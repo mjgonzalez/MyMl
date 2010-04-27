a6e6fc24eff3879548f0fbb79d936fe0516b0b18
nsuarezmeli <nicolas.suarez@mercadolibre.com>
2010-04-27 17:30:23 -0300

    Agregado de mas tests

--- test/integration/myml/QuestionServiceTest.groovy
+++ test/integration/myml/QuestionServiceTest.groovy
@@ -11,36 +11,35 @@ class QuestionServiceTest extends GroovyTestCase{
 	 * Testea que si el usuario no tiene preguntas, el servicio no devuelva nada
 	 */
 	void testEmptyQuestions(){
-		Question.findAllBySellerId(1)*.delete
+		def sellerId = 1
+		
+		deleteAllQuestions(sellerId)
 				
-		assertTrue questionService.getPendingQuestions(1).isEmpty()
+		assertNotHasPendingQuestions(sellerId)
 	}
 
 	/*
 	 * Testeo responder pregunta
 	 */
 	void testNotPendingQuestions(){
-//		mockDomain(Question)
-//		mockDomain(Answer)
-//		mockDomain(QuestionAction)
-//		mockDomain(Item)
-		
-		Question.findAllBySellerId(1)*.delete
+		def sellerId = 2
+		deleteAllQuestions(sellerId)
 		
 		Item testItem = new Item(siteId:"MLA", title:"Item de testeo")
 		testItem.save()
 		
 		Question q = new Question()
-		q.sellerId = 2
+		q.sellerId = sellerId
 		q.askerNickname = "AGILE"
 		q.questionText = "Tenes stock?"
 		q.item = testItem
 		
 		q.save()
 		
-		assertFalse questionService.getPendingQuestions(2).isEmpty()
+		assertHasPendingQuestions(sellerId)
 		
 		String answerText = "Todo bien vos?"
+		
 		questionService.answerQuestion(q.id, answerText)
 		
 		QuestionAction answerAction = QuestionAction.findByQuestion(q)
@@ -50,5 +49,48 @@ class QuestionServiceTest extends GroovyTestCase{
 		Answer answer = Answer.findByQuestion(q)
 		assertNotNull answer
 		assertEquals answerText, answer.answerText
+		
+		assertNotHasPendingQuestions(sellerId)
+	}
+	
+	/*
+	 * Testeo eliminar una pregunta
+	 */
+	void testDeletePendingQuestion(){
+		def sellerId = 2
+		deleteAllQuestions(sellerId)
+		
+		Item testItem = new Item(siteId:"MLA", title:"Item de testeo")
+		testItem.save()
+		
+		Question q = new Question()
+		q.sellerId = sellerId
+		q.askerNickname = "AGILE"
+		q.questionText = "Haces envios?"
+		q.item = testItem
+		
+		q.save()
+		
+		assertHasPendingQuestions(sellerId)
+		
+		questionService.deleteQuestion (q.id)
+		
+		QuestionAction answerAction = QuestionAction.findByQuestion(q)
+		assertNotNull answerAction
+		assertEquals "DELETE", answerAction.actionType
+		
+		assertNotHasPendingQuestions(sellerId)
+	}
+	
+	boolean deleteAllQuestions(Long sellerId){
+		Question.findAllBySellerId(sellerId)*.delete
+	}
+	
+	boolean assertNotHasPendingQuestions(Long sellerId){
+		assertTrue questionService.getPendingQuestions(sellerId).isEmpty()
+	}
+	
+	boolean assertHasPendingQuestions(Long sellerId){
+		assertFalse questionService.getPendingQuestions(sellerId).isEmpty()
 	}
 }
