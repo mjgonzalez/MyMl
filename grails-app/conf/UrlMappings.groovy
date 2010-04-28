6d4552d7869e89bf34a57a407b9f19e48d1241f1
nsuarezmeli <nicolas.suarez@mercadolibre.com>
2010-04-28 12:56:57 -0300

    Se agrega algo de Rest para ejecutar metodos de los Domain

--- grails-app/conf/UrlMappings.groovy
+++ grails-app/conf/UrlMappings.groovy
@@ -5,6 +5,14 @@ class UrlMappings {
 			 // apply constraints here
 		  }
 	  }
+      "/rest/$type/$domain/$id?"{
+    	  controller = "rest"
+    	  action = [GET:"show", PUT:"create", POST:"update", DELETE:"delete"]
+    	  constraints {
+    		  		type(inList:["xml","json"])
+    	  }
+      }
+      
       "/"(view:"/index")
 	  "500"(view:'/error')
 	}
