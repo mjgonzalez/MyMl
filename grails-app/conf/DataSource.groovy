6119d8361b067e43e04e8eeea38d9d5388007ff4
pdigregorio <pdigregorio@ubuntu.ubuntu-domain>
2010-04-29 17:43:07 -0300

    Modifique el datasource para que use db en memoria para los contextos de test y ci

--- grails-app/conf/DataSource.groovy
+++ grails-app/conf/DataSource.groovy
@@ -1,37 +1,40 @@
 hibernate {
-    cache.use_second_level_cache=true
-    cache.use_query_cache=true
-    cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
+	cache.use_second_level_cache=false
+	cache.use_query_cache=false
+	cache.provider_class='net.sf.ehcache.hibernate.EhCacheProvider'
 }
 // environment specific settings
 environments {
 	ci {
-	     dataSource {
-		dbCreate = "create-drop"
-		url = "jdbc:hsqldb:mem:ciDB"
-             }
-    	   }
+		dataSource {
+			pooled = true
+			driverClassName = "org.hsqldb.jdbcDriver"
+			username = "sa"
+			password = ""
+			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+			url = "jdbc:hsqldb:mem:ciDB"
+		}
+	}
 	development {
 		dataSource {
 			driverClassName = "oracle.jdbc.OracleDriver"
 			dialect='org.hibernate.dialect.Oracle9iDialect'
 			username = "nsuarez"
 			password = "oracle"
-//			dbCreate = "create" // one of 'create', 'create-drop','update'
+			//			dbCreate = "create" // one of 'create', 'create-drop','update'
 			url = "jdbc:oracle:thin:@10.0.10.22:1521:desa"
-//			pooled = true
+			//			pooled = true
 		}
 	}
 	
 	test {
 		dataSource {
-			driverClassName = "oracle.jdbc.OracleDriver"
-			dialect='org.hibernate.dialect.Oracle9iDialect'
-			username = "nsuarez"
-			password = "oracle"
-			dbCreate = "create" // one of 'create', 'create-drop','update'
-			url = "jdbc:oracle:thin:@10.0.10.22:1521:desa"
-//			pooled = true
+			pooled = true
+			driverClassName = "org.hsqldb.jdbcDriver"
+			username = "sa"
+			password = ""
+			dbCreate = "create-drop" // one of 'create', 'create-drop','update'
+			url = "jdbc:hsqldb:mem:testDB"
 		}
 	}
 }
