7d54cd6357e2ebdf018a6b9157d7ecad1cd9b2c0
pdigregorio <pdigregorio@ubuntu.ubuntu-domain>
2010-05-05 17:16:58 -0300

    MYML-18 ; removido que era innecesario

--- grails-app/services/myml/ItemService.groovy
+++ grails-app/services/myml/ItemService.groovy
@@ -4,7 +4,7 @@ class ItemService {
 	def getSellerItems = {sellerId ->
 		println Customer.list()
 		Item.findAllBySeller(Customer.get(sellerId))	
-	}		
+	}
 
 	def getSellerActiveItems = {sellerId ->
 		Item.findAllBySellerAndStatus(Customer.get(sellerId), "A")			
