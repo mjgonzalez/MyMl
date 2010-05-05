934f7b9765dcc2ef0b680d1e0efadab1f68a3779
nsuarezmeli <nicolas.suarez@mercadolibre.com>
2010-05-05 17:25:48 -0300

    MYML-18 se hace un poco mas lindo el html de la pagina

--- grails-app/views/activeItems/list.gsp
+++ grails-app/views/activeItems/list.gsp
@@ -5,36 +5,52 @@
 
 <body>
 	<g:if test="${sellerItems}">
+		<table>
+				<tr>
+					<td>Foto</td>
+					<td>Título</td>
+					<td>Precio</td>
+					<td>Cant. Vendida</td>
+					<td>Ofertas</td>
+					<td>Disponible</td>
+				</tr>
 		<g:each var="i" in="${sellerItems}">
-			<div id="div${i.id}">
-				<img src="${i.mainImageURL}"/>
-				<a href="${i.vipURL}">${i.title}(${i.id})</a>
-				<div id="currentPrice${i.id}">${i.currentPrice}</div>
-				<div id="quantitySelled${i.id}">${i.quantitySelled}</div>
-				<div id="offersReceived${i.id}">${i.offersReceived}</div>
-				<div id="quantityAvailable${i.id}">${i.quantityAvailable}</div>	
-			</div>
-			<div>
-				<table>
-					<tbody>
-						<tr>
-							<td height="20">
-								<a href="" class="hpepig">Modificar!</a>
-							</td>
-							<td height="20">
-								<a href="" class="hpepig">Publicar ítem similar</a>
-							</td>
-							<td height="20">
-								<a href="" class="hpepig">¡Cerrar Ya!</a>
-							</td>
-							<td height="20">
-								<a href="" class="hpepig">Pausar venta</a>
-							</td>
-						</tr>
-					</tbody>
-				</table>
-			</div>
+			<tr>
+				<td>	
+					<img src="${i.mainImageURL}"/>
+				</td>
+				<td>
+					<a href="${i.vipURL}">${i.title}(${i.id})</a>
+				</td>
+				<td id="currentPrice${i.id}">${i.currentPrice}</td>
+				<td id="quantitySelled${i.id}">${i.quantitySelled}</td>
+				<td id="offersReceived${i.id}">${i.offersReceived}</td>
+				<td id="quantityAvailable${i.id}">${i.quantityAvailable}</td>
+				<td>	
+					<div>
+						<table>
+							<tbody>
+								<tr>
+									<td height="20">
+										<a href="" class="hpepig">Modificar!</a>
+									</td>
+									<td height="20">
+										<a href="" class="hpepig">Publicar ítem similar</a>
+									</td>
+									<td height="20">
+										<a href="" class="hpepig">¡Cerrar Ya!</a>
+									</td>
+									<td height="20">
+										<a href="" class="hpepig">Pausar venta</a>
+									</td>
+								</tr>
+							</tbody>
+						</table>
+					</div>
+				</td>
+			</tr>
 		</g:each>
+		</table>
 	</g:if>
 	<g:else>
 		<g:message code="items.activeItems.empty"/>
--- test/webtest/myml/SellerActiveItemsWebTests.groovy
+++ test/webtest/myml/SellerActiveItemsWebTests.groovy
@@ -5,6 +5,8 @@ class SellerActiveItemsWebTests extends grails.util.WebTest{
 	def sellerId
 
     void setUp(){
+		super.setUp()
+		
     	Customer c = new Customer(nickname:"TEST1")
     	
     	c.save()
@@ -12,13 +14,13 @@ class SellerActiveItemsWebTests extends grails.util.WebTest{
     	sellerId = c.id
     	
     	sellerItems = [new Item(title:"Libro Analisis Y Diseño De Sistemas De Kendall Y Kendall", siteId:"MLA", seller:c, status:"A", 
-    							vipURL:"http://www.mercadolibre.com.ar/jm/item?site=MLA&id=84213167",
-    							mainImageURL:"http://img1.mlstatic.com/jm/img?s=MLA&f=84213167_3249.jpg&v=I", currentPrice:75,
-    				    		quantitySelled:5, offersReceived:1,quantityAvailable:2),
-    	                   new Item(title:"Notebook Hp 14,1 Serie Dv2000 Modelo Dv2320la", siteId:"MLA", seller:c, status:"A", 
-    	                		vipURL:"http://www.mercadolibre.com.ar/jm/item?site=MLA&id=84214347",
-    	                		mainImageURL:"http://img1.mlstatic.com/jm/img?s=MLA&f=84214347_300.jpg&v=I", currentPrice:2400,
-    	                		quantitySelled:0, offersReceived:1,quantityAvailable:1)]
+							vipURL:"http://www.mercadolibre.com.ar/jm/item?site=MLA&id=84213167",
+							mainImageURL:"http://img1.mlstatic.com/jm/img?s=MLA&f=84213167_3249.jpg&v=I", currentPrice:75,
+				    		quantitySelled:5, offersReceived:1,quantityAvailable:2),
+	                   new Item(title:"Notebook Hp 14,1 Serie Dv2000 Modelo Dv2320la", siteId:"MLA", seller:c, status:"A", 
+	                		vipURL:"http://www.mercadolibre.com.ar/jm/item?site=MLA&id=84214347",
+	                		mainImageURL:"http://img1.mlstatic.com/jm/img?s=MLA&f=84214347_300.jpg&v=I", currentPrice:2400,
+	                		quantitySelled:0, offersReceived:1,quantityAvailable:1)]
     	
     	sellerItems*.save()
     }
@@ -42,10 +44,10 @@ class SellerActiveItemsWebTests extends grails.util.WebTest{
 
     		verifyXPath xpath:"//img[@src='$it.mainImageURL']"
     		verifyXPath xpath:"//a[@href='$it.vipURL']"
-    		verifyXPath xpath:"//div[@id='currentPrice$it.id']", text:it.currentPrice
-    		verifyXPath xpath:"//div[@id='quantitySelled$it.id']", text:it.quantitySelled
-    		verifyXPath xpath:"//div[@id='offersReceived$it.id']", text: it.offersReceived
-    		verifyXPath xpath:"//div[@id='quantityAvailable$it.id']", text:it.quantityAvailable
+    		verifyXPath xpath:"//td[@id='currentPrice$it.id']", text:it.currentPrice
+    		verifyXPath xpath:"//td[@id='quantitySelled$it.id']", text:it.quantitySelled
+    		verifyXPath xpath:"//td[@id='offersReceived$it.id']", text: it.offersReceived
+    		verifyXPath xpath:"//td[@id='quantityAvailable$it.id']", text:it.quantityAvailable
     	}
     }
 }
