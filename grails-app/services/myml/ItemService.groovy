7f553ae599d903de4234708823dab2d34fd6155b
mgonzalez <mgonzalez@ubuntu.ubuntu-domain>
2010-05-14 18:07:15 -0300

    User Story: como vendedor debo poder ver mis articulos pendientes

--- grails-app/services/myml/ItemService.groovy
+++ grails-app/services/myml/ItemService.groovy
@@ -4,4 +4,8 @@ class ItemService {
 	def getSellerActiveItems = {sellerId ->
 		Item.findAllBySellerAndStatus(Customer.get(sellerId), "A")			
 	}
+	
+	def getSellerPendingItems = {sellerId ->
+		Item.findAllBySellerAndStatus(Customer.get(sellerId), "P")
+	}
 }
